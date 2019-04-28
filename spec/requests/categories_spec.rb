26b4ece285a9f5277cd8e805332aa1a3b0dc0b79
Wojciech Maniak <wmaniak@gmail.com>
2019-04-28 16:51:13 +0200

    fix indent using space settings_2

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -2,46 +2,45 @@ require "rails_helper"
 
 describe "categories", type: :request do
 
-	let(:categories) { create_list(:category, 5) }
-	let(:parent_category) { create :category, id: 1, category_details: {"1556181010399"=>{"field_name"=>"Test", "field_type"=>"select", "field_data"=>["yes", "not"]}} }
-	let(:category) { create :category, id: 2, parent_id: 1 }
+  let(:categories) { create_list(:category, 5) }
+  let(:parent_category) { create :category, id: 1, category_details: {"1556181010399"=>{"field_name"=>"Test", "field_type"=>"select", "field_data"=>["yes", "not"]}} }
+  let(:category) { create :category, id: 2, parent_id: 1 }
 
-	describe "categories request" do
-		describe "categories list" do
-			it "displays all categories" do
-				categories
-				visit "/categories"
-				expect(page).to have_selector("table tr", count: 6)
-			end
-		end
 
-		describe "when add new categories with parent" do
-			it "have parent categories on categories list" do
-				parent_category
-				category
-				visit "/categories"
-				tr_list = page.find_all(:css, 'tr')
-				tr_with_category = tr_list[2]
-				expect(tr_with_category).to have_css('td.category-parent-id', text: parent_category.id )
-			end
+  describe "categories list" do
+    it "displays all categories" do
+      categories
+      visit "/categories"
+      expect(page).to have_selector("table tr", count: 6)
+    end
+  end
 
-			it "have parent name when edit" do
-				parent_category
-				category
-				visit "/categories/#{category.id}/edit"
-				expect(page.find("#category_parent_id")).to have_text( parent_category.name )
-			end
-		end
+  describe "when add new categories with parent" do
+    it "have parent categories on categories list" do
+      parent_category
+      category
+      visit "/categories"
+      tr_list = page.find_all(:css, 'tr')
+      tr_with_category = tr_list[2]
+      expect(tr_with_category).to have_css('td.category-parent-id', text: parent_category.id )
+    end
 
-		describe "when add new categories with parent" do
-			it "have parent attributes field when edit" do
-				parent_category
-				visit "/categories/new"
-				select "#{parent_category.name}", from: 'category_parent_id'
-				fill_in "category_name", with: "Home and Garden"
-				click_button("Create")
-				expect(page).to have_text("category was successfully created")
-			end
-		end
-	end
+    it "have parent name when edit" do
+      parent_category
+      category
+      visit "/categories/#{category.id}/edit"
+      expect(page.find("#category_parent_id")).to have_text( parent_category.name )
+    end
+  end
+
+  describe "when add new categories with parent" do
+    it "have parent attributes field when edit" do
+      parent_category
+      visit "/categories/new"
+      select "#{parent_category.name}", from: 'category_parent_id'
+      fill_in "category_name", with: "Home and Garden"
+      click_button("Create")
+      expect(page).to have_text("category was successfully created")
+    end
+  end
 end
